730d843579722c2fa7126c6f7d6cb7a681b0325b
kremec <klemen.remec@gmail.com>
2024-05-12 01:36:16 +0200

    Temporary save before refactoring EntryBrowserScreen

--- app/src/main/java/com/subbyte/subcinema/entrybrowser/EntryBrowserViewModel.kt
+++ app/src/main/java/com/subbyte/subcinema/entrybrowser/EntryBrowserViewModel.kt
@@ -4,14 +4,24 @@ import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import com.subbyte.subcinema.Screen
 import com.subbyte.subcinema.models.Entry
+import jcifs.config.PropertyConfiguration
+import jcifs.context.BaseContext
+import jcifs.context.CIFSContextWrapper
+import jcifs.smb.NtlmPasswordAuthenticator
+import jcifs.smb.SmbFile
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
+import java.util.Properties
 
 
 class EntryBrowserViewModel() : ViewModel() {
@@ -69,7 +79,41 @@ class EntryBrowserViewModel() : ViewModel() {
                 }
             }
             EntryBrowserType.SMB -> {
+                var files: Array<out SmbFile>? = null
+                viewModelScope.launch {
+                    withContext(Dispatchers.IO) {
+                        Log.d("subcinema", "SMB FILE GETTER")
+                        jcifs.Config.registerSmbURLHandler()
+                        val config = PropertyConfiguration(
+                            Properties().apply {
+                                setProperty("jcifs.smb.client.enableSMB2", "true")
+                            }
+                        )
+                        val smbAuth = NtlmPasswordAuthenticator("**DOMAIN**", "**USERNAME**", "**PASSWORD**")
+                        val smbFile = SmbFile("smb://${smbAuth.username}:${smbAuth.password}@${smbAuth.userDomain}/", CIFSContextWrapper(
+                            BaseContext(config)
+                        ))
+                        if (!smbFile.exists()) {
+                            Log.d("subcinema", "SMB FILE DOESN'T EXIST")
+                        }
+                        smbFile.connect()
+                        files = smbFile.listFiles()
+                    }
+                }
 
+                result.add(Entry(-1, -1, "", ""))
+                var index = 1
+
+                if (files != null) {
+                    for (i in files!!.indices) {
+                        result.add(Entry(index++, i, files!![i].name, files!![i].path))
+                    }
+                }
+                _numOfEntries.value = result.size
+
+                while (result.size % entriesPerPage != 0) {
+                    result.add(Entry(-1, -1, "", ""))
+                }
             }
         }
 
